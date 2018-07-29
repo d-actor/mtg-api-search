5e5c5890411281e3b499a77852fcd73763373406
Dan Actor <danltactor@gmail.com>
2018-07-29 15:49:42 -0600

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
 
 class App extends Component {
+  state = { cards: [], value: '' }
+
+  componentDidMount() {
+    axios.get('https://api.magicthegathering.io/v1/cards')
+      .then( res => {
+        this.setState({ cards: res.data })
+      })
+      .catch ( err => {
+        console.log(err)
+        // throw error
+      })
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  search = () => {
+    const { cards, value } = this.state
+    const regex = new RegExp(value, 'gi')
+    return cards.filter( card =>
+      return card.match(regex)
+    )
+  }
+
+  displayMatches = () => {
+    const matchArray = this.search()
+    return matchArray.map( card =>
+      <li>{ card.name }</li>
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>MTG Search</h1>
+        <input name="value" onChange={this.handleChange} />
+        <ul>
+          { this.displayMatches() }
+        </ul>
       </div>
     );
   }
 }
 
 export default App;
+
